a1a1c718e780c97da0f41984f8070baf0eefd50f
Wilson <wilsonfzheng@gmail.com>
2024-10-05 11:51:49 -0400

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Server running..."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+@app.route('/about', methods=['GET'])
+def about():
+    print("This is the about page")
+
